9e180a99cdc37179087da0eb03858baf1829a801
parkJH <jh.parkssam@gmail.com>
2024-05-07 11:37:45 +0900

    api키 업로드

---
+++ src/api/api.jsx
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC54Jc4ytxTbBAE30k3gRvZTNbSsAvpFrw",
+    authDomain: "jh-shop-f6466.firebaseapp.com",
+    projectId: "jh-shop-f6466",
+    storageBucket: "jh-shop-f6466.appspot.com",
+    messagingSenderId: "430064147100",
+    appId: "1:430064147100:web:bad6755cc6369c70b7eb59",
+    measurementId: "G-QPK69GX4LF"
+  };
