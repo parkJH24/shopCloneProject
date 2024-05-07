3aec32fd7c76ea41766e236bb63f56af92d3806d
parkJH <jh.parkssam@gmail.com>
2024-05-07 11:43:44 +0900

    환경변수 수정

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,11 +1,7 @@
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC54Jc4ytxTbBAE30k3gRvZTNbSsAvpFrw",
-    authDomain: "jh-shop-f6466.firebaseapp.com",
-    projectId: "jh-shop-f6466",
-    storageBucket: "jh-shop-f6466.appspot.com",
-    messagingSenderId: "430064147100",
-    appId: "1:430064147100:web:bad6755cc6369c70b7eb59",
-    measurementId: "G-QPK69GX4LF"
-  };
+    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
